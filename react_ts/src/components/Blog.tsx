3aa41c29318f6c1e4aad2e6d47a3b89844f60dad
rexisn <ndizihiweregis06@gmail.com>
2022-05-31 19:25:00 +0200

    Added post and get requests to a fake json-server

--- react_ts/src/components/Blog.tsx
+++ react_ts/src/components/Blog.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-const Blog : React.FC = () => {
+interface data{
+    
+    title: string,
+    body : string,
+    author : string ,
+}
+
+const Blog : React.FC<data> = ({title , body, author}) => {
     return (
         <div className="rounded-sm  shadow-sm p-2 w-[350px]" >
-            <h1 className="font-bold">Blog title</h1> 
+            <h1 className="font-bold">{title}</h1> 
             <hr className="my-4 border-gray-200"/>
-            <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dicta laboriosam ratione at ducimus fuga veritatis ipsa aspernatur, nam necessitatibus amet. Natus in delectus adipisci iusto earum qui sint vitae?
-            Nostrum facere nemo nihil quidem ut vero dolor quam voluptate eligendi exercitationem. Illo doloribus nam ipsam harum neque asperiores ullam odio obcaecati quasi aliquam, unde deleniti architecto tempora numquam! Nulla?</p>
-            <p className=" text-right mt-4 w-full italic text-sm text-gray-300">Ndahiro Valens Papy</p>
+            <p className="text-sm text-gray-500">{body}</p>
+            <p className=" text-right mt-4 w-full italic text-sm text-gray-300">{author}</p>
             
         </div>
     )
